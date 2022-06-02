5f4751f8ba19d3d491ce2fe981b04ffe52549934
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-01 21:00:08 -0400

    Adjusted ids in aside; commented out section; queryselected elements in JS; added submitBtn Event Listener

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,8 @@
-console.log('hello');
+var inputEl = document.querySelector('#place');
+var submitBtn = document.querySelector('#submit');
+var historyEl = document.querySelector('.history');
+
+submitBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('hello');
+})
