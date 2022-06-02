45bf027b35eb63a2ca6bab7e29185c0abcce79a0
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-01 22:36:36 -0400

    trimed cityInput; called function in event listener; fetched direct geocoding of city in getLocation function

--- assets/script.js
+++ assets/script.js
@@ -12,10 +12,25 @@ function init() {
     }
 }
 
+function getLocation(city) {
+    // Note to self: how to find weather in city with spaces in it (Washington DC, Las Vegas, etc)
+    var newCity = city.replace(/ /g, "");
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + newCity + '&limit=1&appid=062ac5aed23ac309d8aa8d7807a42e70')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function (err) {
+      console.log(err);
+    }); 
+}
+
 submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
 
-    var cityInput = inputEl.value;
+    var cityInput = inputEl.value.trim();
     var historyBtn = document.createElement('button');
     historyBtn.textContent = cityInput;
     historyEl.appendChild(historyBtn);
@@ -24,6 +39,7 @@ submitBtn.addEventListener('click', function (event) {
     srcHistory.push(cityInput);
     localStorage.setItem('srcHistory', JSON.stringify(srcHistory));
 
+    getLocation(cityInput);
     inputEl.value = '';
 })
 
