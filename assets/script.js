c3ee33dcb0119e877ecc0520ac336413a181e3c7
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-01 21:51:55 -0400

    Created eventlistener for historyEl buttons; formatted JS document

--- assets/script.js
+++ assets/script.js
@@ -5,17 +5,17 @@ var historyEl = document.querySelector('.history');
 var srcHistory = JSON.parse(localStorage.getItem('srcHistory')) || [];
 
 function init() {
-    for (var search of srcHistory){
+    for (var search of srcHistory) {
         var historyBtn = document.createElement('button');
         historyBtn.textContent = search;
         historyEl.appendChild(historyBtn);
     }
 }
 
-submitBtn.addEventListener('click', function(event){
+submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
-    
-    var cityInput = inputEl.value; 
+
+    var cityInput = inputEl.value;
     var historyBtn = document.createElement('button');
     historyBtn.textContent = cityInput;
     historyEl.appendChild(historyBtn);
@@ -25,6 +25,14 @@ submitBtn.addEventListener('click', function(event){
     localStorage.setItem('srcHistory', JSON.stringify(srcHistory));
 
     inputEl.value = '';
+})
+
+historyEl.addEventListener('click', function (event) {
+    event.preventDefault();
+    var historyBtn = event.target;
+    if (historyBtn.matches('button')) {
+        console.log(historyBtn.textContent);
+    }
 })
 
 init();
