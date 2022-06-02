a2ec583cc2904966d504d533f86231bf540e3a5c
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-02 14:49:02 -0400

    assigned variables for relevant data; moved cityTitle assignment to getWeather; created forloop to display weatherData; created elements for label and button for uvIndex

--- assets/script.js
+++ assets/script.js
@@ -7,43 +7,55 @@ var cityTitle = document.querySelector('#city');
 var srcHistory = JSON.parse(localStorage.getItem('srcHistory')) || [];
 
 function init() {
-    for (var search of srcHistory) {
-        var historyBtn = document.createElement('button');
-        historyBtn.textContent = search;
-        historyEl.appendChild(historyBtn);
-    }
+  for (var search of srcHistory) {
+    var historyBtn = document.createElement('button');
+    historyBtn.textContent = search;
+    historyEl.appendChild(historyBtn);
+  }
 }
 
 function getLocation(city) {
-    cityTitle.textContent = city;
-    var newCity = city.replace(/ /g, "+");
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + newCity + '&limit=1&appid=062ac5aed23ac309d8aa8d7807a42e70')
-    .then(function(response) {
+  var newCity = city.replace(/ /g, "+");
+  fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + newCity + '&limit=1&appid=062ac5aed23ac309d8aa8d7807a42e70')
+    .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-        getWeather(data[0].lat, data[0].lon);
+      getWeather(city, data[0].lat, data[0].lon);
     })
     .catch(function (err) {
       console.log(err);
-    }); 
+    });
 }
 
-function getWeather(lat, lon) {
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&units=imperial&appid=062ac5aed23ac309d8aa8d7807a42e70')
-    .then(function(response) {
+function getWeather(city, lat, lon) {
+  fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&units=imperial&appid=062ac5aed23ac309d8aa8d7807a42e70')
+    .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-    //   Remember to display an icon for the weather data.current.weather[0].icon
-      var weatherData = {
-        temp: data.current.temp,
-        wind: data.current.wind_speed,
-        humidity: data.current.humidity,
-        uvi: data.current.uvi,
-      };
-      console.log(weatherData);
+
+      var weatherIcon = data.current.weather[0].icon;
+      // TODO: Remember to display icon
+      var weatherData = [
+        'Temp: ' + data.current.temp + "°F",
+        'Wind: ' + data.current.wind_speed + ' mph',
+        'Humidity: ' + data.current.humidity + '%'
+      ];
+      var uvIndex = data.current.uvi;
+
+      cityTitle.textContent = city;
+      for (var item of weatherData ) {
+        var weatherItem = document.createElement('p');
+        weatherItem.textContent = item;
+        weatherEl.appendChild(weatherItem);
+      }
+      var uvLabel = document.createElement('p');
+      var uvEl = document.createElement('button');
+      uvLabel.textContent = 'UV index: ';
+      uvEl.textContent = uvIndex;
+      weatherEl.append(uvLabel, uvEl);
     })
     .catch(function (err) {
       console.log(err);
@@ -51,27 +63,27 @@ function getWeather(lat, lon) {
 }
 
 submitBtn.addEventListener('click', function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var cityInput = inputEl.value.trim();
-    var historyBtn = document.createElement('button');
-    historyBtn.textContent = cityInput;
-    historyEl.appendChild(historyBtn);
+  var cityInput = inputEl.value.trim();
+  var historyBtn = document.createElement('button');
+  historyBtn.textContent = cityInput;
+  historyEl.appendChild(historyBtn);
 
-    var srcHistory = JSON.parse(localStorage.getItem('srcHistory')) || [];
-    srcHistory.push(cityInput);
-    localStorage.setItem('srcHistory', JSON.stringify(srcHistory));
+  var srcHistory = JSON.parse(localStorage.getItem('srcHistory')) || [];
+  srcHistory.push(cityInput);
+  localStorage.setItem('srcHistory', JSON.stringify(srcHistory));
 
-    getLocation(cityInput);
-    inputEl.value = '';
+  getLocation(cityInput);
+  inputEl.value = '';
 })
 
 historyEl.addEventListener('click', function (event) {
-    event.preventDefault();
-    var historyBtn = event.target;
-    if (historyBtn.matches('button')) {
-        getLocation(historyBtn.textContent);
-    }
+  event.preventDefault();
+  var historyBtn = event.target;
+  if (historyBtn.matches('button')) {
+    getLocation(historyBtn.textContent);
+  }
 })
 
 init();
